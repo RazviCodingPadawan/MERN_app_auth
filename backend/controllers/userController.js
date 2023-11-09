ee1f9361e4a50fc557738e290e9b3f1893111f20
RazviCodingPadawan <razvan.georgescu89@yahoo.com>
2023-11-10 00:45:57 +0100

    signing up and hashing passwords

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,4 @@
-const User=require('../models/userModel')
+const User = require("../models/userModel");
 
 // login user
 const loginUser = async (req, res) => {
@@ -7,7 +7,17 @@ const loginUser = async (req, res) => {
 
 // signup user
 const signupUser = async (req, res) => {
-  res.json({ mssg: "signup user" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+
+  // res.json({ mssg: "signup user" });
 };
 
 module.exports = {
