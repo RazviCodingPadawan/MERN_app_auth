5bd7e631bdd5bd5cc782776f220391b078800226
RazviCodingPadawan <razvan.georgescu89@yahoo.com>
2023-11-11 15:37:27 +0100

    react auth context added

---
+++ frontend/src/hooks/useAuthContext.js
@@ -0,0 +1,12 @@
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
+
+export const useAuthContext = () => {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw Error("useAuthContext must be used insinde an AuthContextProvider");
+  }
+
+  return context;
+};
